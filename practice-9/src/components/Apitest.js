6dcffdc69497bdacabf3bedacb86e1af017590e6
Nhoj <nhojplummer@yahoo.com>
2023-04-18 15:41:55 -0400

    Making progress on the test

--- practice-9/src/components/Apitest.js
+++ practice-9/src/components/Apitest.js
@@ -4,7 +4,11 @@ import "./Apitest.css"
 const Apitest = () => {
   // const [data, setData] = useState([])
 
-  return <div>Hello World</div>
+  return <div className="data__container">
+    <img />
+    <h4>Testing Title</h4>
+    <p>Testing Text</p>
+  </div>
 }
 
 export default Apitest
