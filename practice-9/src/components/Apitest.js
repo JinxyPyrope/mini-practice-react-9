9429c7fcfece235e7644381ef2616cc47850217a
Nhoj <nhojplummer@yahoo.com>
2023-04-18 16:14:19 -0400

    Trying to setup data in map method

--- practice-9/src/components/Apitest.js
+++ practice-9/src/components/Apitest.js
@@ -1,14 +1,25 @@
 import React from "react"
 import "./Apitest.css"
+import { data } from "./data.js"
 
-const Apitest = () => {
-  // const [data, setData] = useState([])
-
-  return <div className="data__container">
+const listData = data.map(test => (
+  <div className="data__container">
     <img />
     <h4>Testing Title</h4>
     <p>Testing Text</p>
   </div>
+))
+
+const Apitest = () => {
+  // const [data, setData] = useState([])
+
+  return (
+    <div className="data__container">
+      <img />
+      <h4>Testing Title</h4>
+      <p>Testing Text</p>
+    </div>
+  )
 }
 
 export default Apitest
