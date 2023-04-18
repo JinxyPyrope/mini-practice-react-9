2194a53b7d253e9703b7942a6d1fb6df33cf8e8b
Nhoj <nhojplummer@yahoo.com>
2023-04-18 16:20:28 -0400

    Will continue th ework tommorrow

--- practice-9/src/components/Apitest.js
+++ practice-9/src/components/Apitest.js
@@ -1,21 +1,13 @@
 import React from "react"
 import "./Apitest.css"
-import { data } from "./data.js"
 
-const listData = data.map(test => (
-  <div className="data__container">
-    <img />
-    <h4>Testing Title</h4>
-    <p>Testing Text</p>
-  </div>
-))
 
 const Apitest = () => {
   // const [data, setData] = useState([])
 
   return (
     <div className="data__container">
-      <img />
+      <img alt="test" />
       <h4>Testing Title</h4>
       <p>Testing Text</p>
     </div>
