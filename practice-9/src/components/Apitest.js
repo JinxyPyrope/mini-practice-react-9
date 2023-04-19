5acbf61cf7ec66b2c93aad4d3898ba258e4c140b
Nhoj <nhojplummer@yahoo.com>
2023-04-19 14:20:56 -0400

    Trying to add more layers for the component

--- practice-9/src/components/Apitest.js
+++ practice-9/src/components/Apitest.js
@@ -1,15 +1,21 @@
-import React from "react"
+import React, { useEffect } from "react"
 import "./Apitest.css"
-
 
 const Apitest = () => {
   // const [data, setData] = useState([])
 
+  // useEffect(() => {
+
+  // })
+
   return (
-    <div className="data__container">
-      <img alt="test" />
-      <h4>Testing Title</h4>
-      <p>Testing Text</p>
+    <div className="data__card-container">
+      <div className="data_card-container__front-end">
+        <img alt="test" title="More Information Here" />
+        <h4>Testing Title</h4>
+        <p>Testing Text</p>
+      </div>
+      <div className="data_card-container__back-end"></div>
     </div>
   )
 }
