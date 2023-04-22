ead05538326e1b2ea5a05e3afb7fd4f122cb8de6
Nhoj <nhojplummer@yahoo.com>
2023-04-22 16:01:15 -0400

    Possibly getting a solution

--- practice-9/src/components/Apitest.js
+++ practice-9/src/components/Apitest.js
@@ -4,28 +4,36 @@ import "./Apitest.css"
 const Apitest = () => {
   const [users, setUsers] = useState([])
 
-  const fetchUserData = () => {
+  // const fetchUserData = () => {
+  //   fetch("https://random-data-api.com/api/users/random_user?size=10")
+  //     .then(res => {
+  //       return res.json()
+  //     })
+  //     .then(data => {
+  //       setUsers({ data })
+  //       Object.entries(users).map(x => console.log(x))
+  //     })
+  //     .catch(error => console.log(error))
+  // }
+
+  useEffect(() => {
     fetch("https://random-data-api.com/api/users/random_user?size=10")
       .then(res => {
         return res.json()
       })
       .then(data => {
-        console.log(data)
-        setUsers({ data })
+        setUsers(data)
+        Object.entries(users).map(x => console.log(x))
       })
       .catch(error => console.log(error))
-  }
-
-  useEffect(() => {
-    fetchUserData()
-  }, [])
+  }, [users])
 
   return (
     <div className="data__card-container">
       <div className="data_card-container__front-end">
-        {users.map((list, index) => (
-          <img  src={list.avatar} alt="test" title="More Information Here" />
-        ))}
+        {/* {Object.entries(users).map((list, index) => (
+          <img src={list.avatar} alt="test" title="More Information Here" />
+        ))} */}
         {/* <img alt="test" title="More Information Here" />
         <h4>Testing Title</h4>
         <p>Testing Text</p> */}
