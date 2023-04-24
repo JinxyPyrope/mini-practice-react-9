25d4ee995ae93fcd311b19c4c2bec8bc56183a1c
Nhoj <nhojplummer@yahoo.com>
2023-04-24 12:34:59 -0400

    Trying out a new angle

--- practice-9/src/App.js
+++ practice-9/src/App.js
@@ -1,7 +1,25 @@
 import "./App.css"
 import Apitest from "./components/Apitest"
+import React, { useState, useEffect } from "react"
+
+
 
 function App() {
+
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    fetch("https://random-data-api.com/api/users/random_user?size=10")
+      .then(res => {
+        return res.json()
+      })
+      .then(data => {
+        console.log(data)
+        setUsers(data)
+      })
+  }, [])
+
+  
   return (
     <div className="App">
       <Apitest />
--- practice-9/src/components/Apitest.js
+++ practice-9/src/components/Apitest.js
@@ -2,58 +2,21 @@ import React, { useState, useEffect } from "react"
 import "./Apitest.css"
 
 const Apitest = () => {
-  const [users, setUsers] = useState([])
-
-  // const fetchUserData = () => {
-  //   fetch("https://random-data-api.com/api/users/random_user?size=10")
-  //     .then(res => {
-  //       return res.json()
-  //     })
-  //     .then(data => {
-  //       setUsers({ data })
-  //       Object.entries(users).map(x => console.log(x))
-  //     })
-  //     .catch(error => console.log(error))
-  // }
-
-  // useEffect(() => {
-  //   fetch("https://random-data-api.com/api/users/random_user?size=10")
-  //     .then(res => {
-  //       return res.json()
-  //     })
-  //     .then(data => {
-  //       setUsers(data)
-  //       console.log(users)
-  //       // Object.entries(users).map(x => console.log(x))
-  //     })
-  //     .catch(error => console.log(error))
-  // }, [users])
-
-  const fetchUserData = () => {
-    fetch("https://random-data-api.com/api/users/random_user?size=10")
-      .then(res => {
-        return res.json()
-      })
-      .then(data => {
-        setUsers(data)
-        console.log(users)
-      })
-      .catch(error => console.log(error))
-  }
-
-  useEffect(() => {
-    fetchUserData()
-  }, [])
+  
 
   return (
     <div className="data__card-container">
-      <div className="data_card-container__front-end">
-        <img alt="test" title="More Information Here" />
+      
+        {users.map((card) => (
+          <div className="data_card-container__front-end" key={card.id}>
+            <img alt="test" title="More Information Here" />
         <h4>Testing Title</h4>
         <p>Testing Text</p>
+            </div>
+        ))}
       </div>
-      {/* <div className="data_card-container__back-end"></div> */}
-    </div>
+      // {/* <div className="data_card-container__back-end"></div> */}
+   
   )
 }
 
