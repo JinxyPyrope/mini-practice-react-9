26f6c17c835333714fe1d145616d91c364108d93
Nhoj <nhojplummer@yahoo.com>
2023-04-28 15:22:33 -0400

    Finally got the button workin

--- practice-9/src/App.js
+++ practice-9/src/App.js
@@ -5,7 +5,7 @@ import React, { useState, useEffect } from "react"
 function App() {
   const [users, setUsers] = useState([])
 
-  useEffect(() => {
+  const fetchData = async () => {
     fetch("https://random-data-api.com/api/users/random_user?size=10")
       .then(res => {
         return res.json()
@@ -13,13 +13,16 @@ function App() {
       .then(data => {
         setUsers(data)
       })
-  }, [])
+  }
 
+  useEffect(() => {
+    fetchData()
+  }, [])
 
   return (
     <div className="App">
       <div className="btn-container">
-        <button>Fetch Random</button>
+        <button onClick={fetchData}>Fetch Random</button>
       </div>
 
       <Apitest users={users} />
