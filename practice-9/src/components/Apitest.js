3327c325c081309f8da7969f4eb0f4899bf484ce
Nhoj <nhojplummer@yahoo.com>
2023-04-30 13:18:53 -0400

    Starting process on the final feature the card flip

--- practice-9/src/components/Apitest.js
+++ practice-9/src/components/Apitest.js
@@ -3,19 +3,22 @@ import "./Apitest.css"
 
 const Apitest = props => {
   const users = props.users
-  console.log(props, users)
+  // console.log(props, users)
 
   return (
     <div className="data__container">
       {users.map(card => (
         <div className="data__container-card" key={card.id}>
-          <div className="data__container-card__front-end__image">
-            <img src={card.avatar} alt="test" title="More Information Here" />
+          <div className="data__container-card__front-end">
+            <div className="data__container-card__front-end__image">
+              <img src={card.avatar} alt="test" title="More Information Here" />
+            </div>
+            <div className="data__container-card__front-end__text">
+              <h4>{card.first_name}</h4>
+              <p>{card.employment.key_skill}</p>
+            </div>
           </div>
-          <div className="data__container-card__front-end__text">
-            <h4>{card.first_name}</h4>
-            <p>{card.employment.key_skill}</p>
-          </div>
+          <div className="data_card-container__back-end"></div>
         </div>
       ))}
     </div>
